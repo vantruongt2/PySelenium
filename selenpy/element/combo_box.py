9443f35a615f50da4b03dd6ff19e807430f2237f
trungtranjj <49933154+trungtranjj@users.noreply.github.com>
2019-05-07 13:49:25 +0700

    improve combo_box element (#3)
    * improve combo_box element
    
    * remove method select_dynamic_menu

--- selenpy/element/combo_box.py
+++ selenpy/element/combo_box.py
@@ -7,23 +7,37 @@ class ComboBox(BaseElement):
 
     def __init__(self, locator):
         super().__init__(locator)    
-    
+
     @property
     def _select(self):
-        if self._s is None:
-            self._s = Select(self.find_element())
+        # if self._s is None:
+        self._s = Select(self.find_element())
         return self._s
+
+    def is_checked(self):
+        return self._select().is_selected()
 
     def select_by_value(self, value):        
         self._select.select_by_value(value)
-        
+
     def select_by_index(self, idx):    
         self._select.select_by_index(idx)
-        
+
     def select_by_visible_text(self, text):
         self._select.select_by_visible_text(text)
-    
+
+    def select_by_text_contains(self, text):
+        for o in self._select.options:
+            if text in o.text:
+                o.click()
+                break
+
     @property
     def first_selected_text(self):        
         return self._select.first_selected_option.text
-        
+
+    def get_options(self):
+        ops = []
+        for o in self._select.options:
+            ops.append(o.text)
+        return ops
